a032dbc6be3d248d8bfe8ff484a7714574fde2f1
HieuXuog <62530045+hieutran02grc@users.noreply.github.com>
2022-03-11 15:46:44 +0700

    Add files via upload

---
+++ main-page/script/main.js
@@ -0,0 +1,95 @@
+$(document).ready(function(){
+   setTimeout(function(){
+      $(".carousel-caption-css").show();
+   }, 2000);
+});
+$(document).ready(function(){
+document.getElementById("heart").onclick = function(){
+document.querySelector(".fa-gratipay").style.color = "#E74C3C";
+};
+});
+
+
+
+
+  
+
+// hàm responsive
+
+function myFunction(x) {
+  if (x.matches) { // If media query matches
+    var swiper = new Swiper(".mySwiper", {
+        slidesPerView: 1,
+        spaceBetween: 10,
+        slidesPerGroup: 1,
+        loop: true,
+        loopFillGroupWithBlank: true,
+        pagination: {
+          el: ".swiper-pagination",
+          clickable: true,
+        },
+        navigation: {
+          nextEl: ".swiper-button-next",
+          prevEl: ".swiper-button-prev",
+        },
+      });
+ 
+
+
+
+    var swiper = new Swiper(".mySwiper-1", {
+        slidesPerView: 1,
+        spaceBetween: 30,
+        slidesPerGroup: 1,
+        loop: true,
+        loopFillGroupWithBlank: true,
+        pagination: {
+          el: ".swiper-pagination",
+          clickable: true,
+        },
+        navigation: {
+          nextEl: ".swiper-button-next-1",
+          prevEl: ".swiper-button-prev-1",
+        },
+      });
+  } else {
+   var swiper = new Swiper(".mySwiper", {
+        slidesPerView: 4,
+        spaceBetween: 30,
+        slidesPerGroup: 4,
+        loop: true,
+        loopFillGroupWithBlank: true,
+        pagination: {
+          el: ".swiper-pagination",
+          clickable: true,
+        },
+        navigation: {
+          nextEl: ".swiper-button-next",
+          prevEl: ".swiper-button-prev",
+        },
+      });
+ 
+
+
+
+    var swiper = new Swiper(".mySwiper-1", {
+        slidesPerView: 1,
+        spaceBetween: 30,
+        slidesPerGroup: 1,
+        loop: true,
+        loopFillGroupWithBlank: true,
+        pagination: {
+          el: ".swiper-pagination",
+          clickable: true,
+        },
+        navigation: {
+          nextEl: ".swiper-button-next-1",
+          prevEl: ".swiper-button-prev-1",
+        },
+      });
+  }
+}
+
+  var x = window.matchMedia("(max-width: 1000px)")
+  myFunction(x) // Call listener function at run time
+  x.addListener(myFunction) // Attach listener function on state changes
